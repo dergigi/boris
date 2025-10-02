d10e12b8df9eb8371b4a9cf938afd5bad76b92a2
Gigi <dergigi@pm.me>
2025-10-03 00:43:19 +0200

    feat(ui): link author to search.dergigi.com with npub\n\n- Clickable 'by: <author>' opens profile search in new tab\n- Styles for author link

--- src/components/BookmarkItem.tsx
+++ src/components/BookmarkItem.tsx
@@ -24,6 +24,7 @@ import {
 import { faChevronDown, faChevronUp, faBookOpen } from '@fortawesome/free-solid-svg-icons'
 import { useEventModel } from 'applesauce-react/hooks'
 import { Models } from 'applesauce-core'
+import { npubEncode } from 'nostr-tools/nip19'
 import { IndividualBookmark } from '../types/bookmarks'
 import { formatDate, renderParsedContent } from '../utils/bookmarkUtils'
 import ContentWithResolvedProfiles from './ContentWithResolvedProfiles'
@@ -50,6 +51,7 @@ export const BookmarkItem: React.FC<BookmarkItemProps> = ({ bookmark, index, onS
 
   // Resolve author profile using applesauce
   const authorProfile = useEventModel(Models.ProfileModel, [bookmark.pubkey])
+  const authorNpub = npubEncode(bookmark.pubkey)
   
   // Get display name for author
   const getAuthorDisplayName = () => {
@@ -176,7 +178,15 @@ export const BookmarkItem: React.FC<BookmarkItemProps> = ({ bookmark, index, onS
           <FontAwesomeIcon icon={getKindIcon(bookmark.kind)} />
         </span>
         <span>
-          by: {getAuthorDisplayName()}
+          <a
+            href={`https://search.dergigi.com/p/${authorNpub}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="author-link"
+            title="Open author in search"
+          >
+            by: {getAuthorDisplayName()}
+          </a>
         </span>
       </div>
 
