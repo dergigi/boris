41845615830ea94a906b9ada0f5432fd39c7c40f
Gigi <dergigi@pm.me>
2025-10-07 06:57:42 +0100

    chore: cleanup after build fixes (remove shims, update locks)

--- src/types/applesauce-core-deep.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'applesauce-core/dist/event-store/event-store.js' {
-  // Re-export the EventStore type from the main package so TS has correct types
-  export { EventStore } from 'applesauce-core';
-}
-
-
