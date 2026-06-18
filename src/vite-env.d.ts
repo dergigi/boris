764aedcb027527d301c49d9e0aa02b8940cf4e7c
dergigi <dergigi@users.noreply.github.com>
2026-06-18 13:54:25 +0200

    fix: default home route to explore

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,5 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly VITE_DEFAULT_ARTICLE_NADDR: string
-}
-
 declare module '*.svg?raw' {
   const content: string
   export default content
